39ca30dee6c9dd977b12137df27612d8d3dd66d9
keniiy <kehindekehinde894@gmail.com>
2021-04-06 13:11:34 +0100

    nodemon config

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-const express = require('express');
+// const express = require('express');
+// const app = express();
+
+// app.listen(4000, function()
+// {console.log(`server is running on port 4000`)});
+
+// module.export = app;ca
+
+import express from 'express'
+
 const app = express();
 
-app.listen(4000, function()
-{console.log(`server is running on port 4000`)})
+app.listen(4000,()=> console.log(`Server is running on port 4000`));
 
-module.export = app;
+module.exports = app;
+
